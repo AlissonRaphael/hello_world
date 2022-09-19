dc51c41994efad1a9294f2dc710249b50a149a47
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-09-19 11:13:30 -0300

    feat: typing classes

---
+++ typescript/03_class/01_constructor.ts
@@ -0,0 +1,26 @@
+class FristDate {
+  day: number
+  mounth: string
+  year: number
+
+  constructor (day: number = 1, mounth: string = 'jan', year: number = 1989) {
+    this.day = day
+    this.mounth = mounth
+    this.year = year
+  }
+}
+
+const firstDate = new FristDate(7, 'set', 2001)
+console.log(firstDate)
+
+class LastDate {
+  constructor (
+    public day: number = 1,
+    public mounth: string = 'jan',
+    public year: number = 1989
+  ) {}
+}
+
+const lastDate = new LastDate(20, 'out', 2019)
+console.log(lastDate)
+ 
