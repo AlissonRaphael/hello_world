6aca8e36dd9184f3dc7550e5fe6d83945cabceba
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-11-07 20:49:06 -0300

    refactor: fix generics with functions

--- typescript/07_generics/01_functions.ts
+++ typescript/07_generics/01_functions.ts
@@ -8,3 +8,12 @@ echo({
   active: true,
   status: function () {console.log(`${this.name}: ${this.active}`)}
 }).status()
+
+function nextPage <T> (param: T): T {
+  console.log(param)
+  return param
+}
+
+const printPage: <R> (param: R) => R = nextPage
+const newPage = printPage(25)+5
+printPage(newPage)
