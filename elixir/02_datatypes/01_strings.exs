fb25cee74c04d79d97527ab60a6870898e0a21b3
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-11-11 20:45:47 -0300

    feat: IO device

--- elixir/02_datatypes/01_strings.exs
+++ elixir/02_datatypes/01_strings.exs
@@ -2,3 +2,7 @@ fname = "Alisson"
 lname = "Oliveira"
 
 IO.puts("Welcome! Mr. #{fname} #{lname}.")
+
+country = IO.gets("Where you are from #{fname}? ")
+
+IO.puts(fname <> " " <> lname <> " from #{country}")
