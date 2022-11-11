6f35b5db21413bfe4038fc4b6de289f8f4b3c060
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-11-11 20:46:52 -0300

    feat: string manipulation part.1

--- elixir/02_datatypes/01_strings.exs
+++ elixir/02_datatypes/01_strings.exs
@@ -6,3 +6,25 @@ IO.puts("Welcome! Mr. #{fname} #{lname}.")
 country = IO.gets("Where you are from #{fname}? ")
 
 IO.puts(fname <> " " <> lname <> " from #{country}")
+
+String.length(country) # 6
+byte_size("É") # 2
+
+String.at("North East", 6) # E
+String.at("North East", 0) # N
+String.at("North East", 2) # r
+String.at("North East", -1) # t
+String.at("North East", -6) # h
+
+String.bag_distance("abc", "") # 0
+String.bag_distance("abcd", "a") # 0.25
+String.bag_distance("abcd", "abcd") # 1
+String.bag_distance("abcd", "ab") # 0.5
+
+String.capitalize("northeast") # Northeast
+String.downcase("NOT NORTH") # not north
+String.codepoints("Hello!") # ["H", "e", "l", "l", "!"]
+
+String.contains?("I live in the northeast", "northeast") # true
+String.contains?("I live in the northeast", ["northeast", "north"]) # true
+String.contains?("I live in the northeast", ["south", "north"]) # false
