bc43649ce38113dba6588711093b28361de81dcd
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-01-05 20:59:46 -0300

    feat: uri manipulation part.2

--- elixir/02_datatypes/06_uri.exs
+++ elixir/02_datatypes/06_uri.exs
@@ -3,6 +3,10 @@ uri = URI.parse("http://example.com/product?size=40&page=2")
 URI.decode("https%3A%2F%2Felixir-lang.org") # "https://elixir-lang.org"
 URI.decode_query("size=40&page=2") # %{"page" => "2", "size" => "40"}
 
-IO.inspect(uri2)
+URI.encode("ftp://s-ite.tld/?value=put it+й") # "ftp://s-ite.tld/?value=put%20it+%D0%B9"
+URI.encode_query(%{"page" => "2", "size" => "40"}) # "size=40&page=2"
+
+uri = URI.char_reserved?("asdf")
+IO.inspect(uri)
 
 System.stop()
