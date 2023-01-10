5bd8d667d9ce7a01822d33c9adf21a4ae4a20d2a
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-01-10 20:26:08 -0300

    feat: uri manipulation part.4

--- elixir/02_datatypes/06_uri.exs
+++ elixir/02_datatypes/06_uri.exs
@@ -1,4 +1,4 @@
-URI.parse("http://example.com/product?size=40&page=2")
+URI.parse("http://example.com/product?size=40&page=2") # %URI{ authority: "example.com", fragment: nil, host: "example.com", path: "/product", port: 80, query: "size=40&page=2", scheme: "http", userinfo: nil }
 
 URI.decode("https%3A%2F%2Felixir-lang.org") # "https://elixir-lang.org"
 URI.decode_query("size=40&page=2") # %{"page" => "2", "size" => "40"}
@@ -16,6 +16,6 @@ URI.char_unescaped?(?{) # false
 URI.char_unescaped?(?_) # true
 URI.char_unescaped?(?+) # true
 
-IO.inspect(uri)
+URI.to_string(URI.merge("http://google.com","/query")) # "http://google.com/query"
 
 System.stop()
