1de1cc76a4ef65cbd8ac7e6d01cf2a066a46974d
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2026-04-25 17:54:41 -0300

    variables

---
+++ cpp/02_variables.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+  // declaration
+  int number;
+  float avg;
+  bool status;
+
+  // attribution
+  number = 10;
+  avg = 8.5;
+  status = false;
+
+  // declaration and attribution
+  int total = 100;
+
+  std::cout<<number<<std::endl;
+  std::cout<<avg<<std::endl;
+  std::cout<<total<<std::endl;
+  std::cout<<status; // true: 1, false: 0
+
+  return 0;
+}
