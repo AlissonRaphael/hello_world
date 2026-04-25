d6332c1e22078d9addd94269d15eb3877b50b5f6
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2026-04-25 17:54:52 -0300

    cin and cout

---
+++ cpp/03_cin_and_cout.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+  int num1;
+  int num2;
+
+  std::cout<<"Enter the number 1: ";
+  std::cin>>num1;
+
+  std::cout<<"Enter the number 2: ";
+  std::cin>>num2;
+
+  std::cout<<"Number 1 = "<<num1<<" and Number 2 = "<<num2;
+
+  return 0;
+}
