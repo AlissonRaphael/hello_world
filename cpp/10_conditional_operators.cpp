9ab67862153ff071956b678561eb6bc4d4c50b92
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2026-04-26 16:02:17 -0300

    conditional operators

---
+++ cpp/10_conditional_operators.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout<<"A && B = C"<<endl;
+  cout<<"0 && 0 = "<<(0 && 0)<<endl;
+  cout<<"1 && 0 = "<<(1 && 0)<<endl;
+  cout<<"1 && 1 = "<<(1 && 1)<<endl;
+  cout<<"0 && 1 = "<<(0 && 1)<<endl;
+  cout<<"----------"<<endl;
+
+  cout<<"A || B = C"<<endl;
+  cout<<"0 || 0 = "<<(0 || 0)<<endl;
+  cout<<"1 || 0 = "<<(1 || 0)<<endl;
+  cout<<"1 || 1 = "<<(1 || 1)<<endl;
+  cout<<"0 || 1 = "<<(0 || 1)<<endl;
+  cout<<"----------"<<endl;
+
+  cout<<"!A = C"<<endl;
+  cout<<"!0 = "<<!0<<endl;
+  cout<<"!1 = "<<!1<<endl;
+
+  return 0;
+}
