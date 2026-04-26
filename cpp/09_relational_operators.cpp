bf1349722a716f33b3beafa94acafd05e8e1a654
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2026-04-26 16:01:59 -0300

    relational operators

---
+++ cpp/09_relational_operators.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  std::cout<<"10 > 5: "<<(10>5)<<std::endl;
+  std::cout<<"10 < 5: "<<(10<5)<<std::endl;
+  std::cout<<"10 == 5: "<<(10==5)<<std::endl;
+  std::cout<<"5 == 5: "<<(5==5)<<std::endl;
+  std::cout<<"5 >= 5: "<<(5>=5)<<std::endl;
+  std::cout<<"5 <= 10: "<<(5<=10)<<std::endl;
+
+  return 0;
+}
