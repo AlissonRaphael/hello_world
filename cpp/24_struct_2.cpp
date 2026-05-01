4d6e7f8a17a5d5454ba652098980099bff723fca
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2026-04-30 23:54:44 -0300

    struct 2

---
+++ cpp/24_struct_2.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct User {
+  int id;
+  string email;
+  char active;
+};
+
+int verify() {
+  char value;
+
+  cout<<"Continue Registering (y/n)? ";
+  cin>>value;
+
+  if (value == 'y') {
+    return 1;
+  }
+
+  return 0;
+};
+
+User save(int *id_count) {
+  User user;
+
+  user.id = *id_count;
+  *id_count = *id_count + 1;
+
+  cout<<"Enther the email: ";
+  cin>>user.email;
+
+  cout<<"Is active (y/n): ";
+  cin>>user.active;
+
+  return user;
+};
+
+int main() {
+  int id_count = 1;
+  int keep = 1;
+  vector<User> users;
+
+  while(keep) {
+    User user = save(&id_count);
+    users.push_back(user);
+    keep = verify();
+  }
+
+  for (int index = 0; index < users.size(); index++) {
+    cout<<"User "<<index+1<<endl;
+    cout<<"- id: "<<users[index].id<<endl;
+    cout<<"- email: "<<users[index].email<<endl;
+    cout<<"- active: "<<users[index].active<<endl;
+  }
+
+  return 0;
+}
