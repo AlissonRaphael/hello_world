42b93d7c9c3254dfa11e9f4c9729eb54ad955012
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2026-05-09 23:54:25 -0300

    implicit_conversion

---
+++ cpp/39_implicit_conversion.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  float pi = 3.14;
+  int pi_number = pi;
+
+  cout << pi_number << endl; // 3
+
+  char key = 'w'; 
+  int key_code = key;
+
+  cout << key_code << endl; // 57
+
+  int number = 10;
+  float float_number = number;
+
+  cout << float_number << endl; // 10
+
+  return 0;
+}
