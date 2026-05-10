e4a0c9c09de52e481aad4709194c08882f69c172
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2026-05-09 23:11:41 -0300

    headers

---
+++ cpp/37_0_headers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "37_1_custom_math.h"
+
+using namespace std;
+
+int main()
+{
+  cout << "3 + 5 = " << addition(3, 5) << endl;
+  cout << "9 - 1 = " << subtraction(9, 1) << endl;
+  cout << "2 x 5 = " << multiplication(2, 5) << endl;
+  cout << "9 / 3 = " << division(9, 3) << endl;
+
+  return 0;
+}
---
+++ cpp/37_1_custom_math.h
@@ -0,0 +1,21 @@
+int addition(int a, int b)
+{
+  return a + b;
+}
+
+int subtraction(int a, int b)
+{
+  return a - b;
+}
+
+float division(int a, int b)
+{
+  float result;
+  result = float(a) / float(b);
+  return result;
+}
+
+int multiplication(int a, int b)
+{
+  return a * b;
+}
