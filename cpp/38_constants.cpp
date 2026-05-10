c0bb0cf25f980e61d396a83b0655050357eba36a
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2026-05-09 23:54:07 -0300

    constants

---
+++ cpp/38_constants.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+#define ENVIRONMENT "production"
+
+int main()
+{
+  cout << ENVIRONMENT << endl;
+
+  const string date = "30 de junho de 2002";
+  cout << date << endl;
+
+  return 0;
+}
